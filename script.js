3d0cb2ad22d7df167f3241f5f8fabd75250f8245
Meredith Jones <mj19003@gmail.com>
2021-11-11 21:35:16 -0500

    Added some move options

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+
+const ticketPrice = +movieSelect.value;
